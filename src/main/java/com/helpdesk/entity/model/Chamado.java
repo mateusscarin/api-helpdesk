e68a8cfc1e89c2af04326b6731d3f2ebeb5c82cb
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-20 11:44:55 -0300

    feat: anotações JPA para as classes de domínio

--- src/main/java/com/helpdesk/entity/model/Chamado.java
+++ src/main/java/com/helpdesk/entity/model/Chamado.java
@@ -1,21 +1,45 @@
 package com.helpdesk.entity.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.helpdesk.entity.enums.Prioridade;
+import com.helpdesk.entity.enums.Status;
 
-import ch.qos.logback.core.status.Status;
+@Entity
+public class Chamado implements Serializable {
 
-public class Chamado {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String observacoes;
+
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado() {
@@ -129,7 +153,5 @@ public class Chamado {
             return false;
         return true;
     }
-
-    
 
 }
--- src/main/java/com/helpdesk/entity/model/Tecnico.java
+++ src/main/java/com/helpdesk/entity/model/Tecnico.java
@@ -3,16 +3,27 @@ package com.helpdesk.entity.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.helpdesk.entity.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa {
 
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {

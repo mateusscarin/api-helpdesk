b79c31b578740ef066abb9171d56b9b19ca5be63
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-21 11:11:10 -0300

    feat: perfil de desenvolvimento

--- src/main/java/com/helpdesk/config/DevConfig.java
+++ src/main/java/com/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,14 +9,21 @@ import org.springframework.context.annotation.Profile;
 import com.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
     @Bean
-    public void instanciaDB(){
-        this.dbService.instanciaDB();
+    public boolean instanciaDB() {
+
+        if (value.equals("create")) {
+            this.dbService.instanciaDB();
+        }
+        return false;
     }
 }

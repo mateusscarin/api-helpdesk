c1970f26e82cf893e60debd107cb30cf99bc9d09
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-24 08:58:26 -0300

    feat: endpoint findAll para Tecnico

--- src/main/java/com/helpdesk/resource/TecnicoResource.java
+++ src/main/java/com/helpdesk/resource/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.helpdesk.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,13 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = tecnicoService.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll(){
+        List<Tecnico> list = tecnicoService.findAll();
+        List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
--- src/main/java/com/helpdesk/services/TecnicoService.java
+++ src/main/java/com/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id) {
         Optional<Tecnico> opt = tecnicoRepository.findById(id);
         return opt.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return tecnicoRepository.findAll();
     }
 }

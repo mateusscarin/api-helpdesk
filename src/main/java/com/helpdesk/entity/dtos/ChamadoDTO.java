57ffe6098800c46b83d478d4573546496ec91a2b
Mateus-Scarin <mateus.scarin@gmail.com>
2022-11-02 16:25:28 -0300

    feat: create para Chamado

--- src/main/java/com/helpdesk/entity/dtos/ChamadoDTO.java
+++ src/main/java/com/helpdesk/entity/dtos/ChamadoDTO.java
@@ -3,6 +3,9 @@ package com.helpdesk.entity.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.helpdesk.entity.model.Chamado;
 
@@ -15,11 +18,17 @@ public class ChamadoDTO implements Serializable {
     private LocalDate dataAbertura = LocalDate.now();
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento = LocalDate.now();
+    @NotNull(message = "O campo PRIORIDADE é requerido!")
     private Integer prioridade;
+    @NotNull(message = "O campo STATUS é requerido!")
     private Integer status;
+    @NotBlank(message = "O campo TITULO é requerido!")
     private String titulo;
+    @NotBlank(message = "O campo OBSERVACOES é requerido!")
     private String observacoes;
+    @NotNull(message = "O campo TECNICO é requerido!")
     private Integer tecnico;
+    @NotNull(message = "O campo CLIENTE é requerido!")
     private Integer cliente;
     private String nomeTecnico;
     private String nomeCliente;
--- src/main/java/com/helpdesk/services/ChamadoService.java
+++ src/main/java/com/helpdesk/services/ChamadoService.java
@@ -6,7 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.helpdesk.entity.dtos.ChamadoDTO;
+import com.helpdesk.entity.enums.Prioridade;
+import com.helpdesk.entity.enums.Status;
 import com.helpdesk.entity.model.Chamado;
+import com.helpdesk.entity.model.Cliente;
+import com.helpdesk.entity.model.Tecnico;
 import com.helpdesk.repository.ChamadoRepository;
 import com.helpdesk.services.exception.ObjectNotFoundException;
 
@@ -16,12 +21,38 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository chamadoRepository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = chamadoRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID : " + id));
     }
 
-    public List<Chamado> findAll(){
+    public List<Chamado> findAll() {
         return chamadoRepository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO objDTO) {
+        return chamadoRepository.save(newChamado(objDTO));
+    }
+
+    private Chamado newChamado(ChamadoDTO chamadoDTO) {
+        Tecnico tecnico = tecnicoService.findById(chamadoDTO.getTecnico());
+        Cliente cliente = clienteService.findById(chamadoDTO.getCliente());
+        Chamado chamado = new Chamado();
+        if (chamadoDTO.getId() != null) {
+            chamado.setId(chamadoDTO.getId());
+        }
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(chamadoDTO.getPrioridade()));
+        chamado.setStatus(Status.toEnum(chamadoDTO.getStatus()));
+        chamado.setTitulo(chamadoDTO.getTitulo());
+        chamado.setObservacoes(chamadoDTO.getObservacoes());
+        return chamado;
     }
 }
